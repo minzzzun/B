175c7433f48c103bae17bbce4fdcd642a062d4c5
minjun kim <minzzzun@gmail.com>
2024-11-13 21:17:10 +0900

    초기 파일 설정

---
+++ moveExTests/moveExTests.swift
@@ -0,0 +1,17 @@
+//
+//  moveExTests.swift
+//  moveExTests
+//
+//  Created by 김민준 on 11/13/24.
+//
+
+import Testing
+@testable import moveEx
+
+struct moveExTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
